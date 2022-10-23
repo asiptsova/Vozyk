d3bf7ccba10afb182686552f3536ec4771a26217
Asiptsova <asipcova@gmail.com>
2022-10-23 19:01:38 +0200

    v1

---
+++ app/src/main/java/com/application/vozyk/Login.java
@@ -0,0 +1,43 @@
+package com.application.vozyk;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        final EditText mEmail = findViewById(R.id.Email);
+        final EditText mPassword = findViewById(R.id.password);
+        final Button mLoginBtn = findViewById(R.id.loginBtn);
+        final TextView  mCreateBtn = findViewById(R.id.createText);
+        final TextView forgotTextLink = findViewById(R.id.forgotPassword);
+        mLoginBtn.setOnClickListener(v -> {
+            String email = mEmail.getText().toString().trim();
+            String password = mPassword.getText().toString().trim();
+
+            if (TextUtils.isEmpty(email)) {
+                mEmail.setError("Email is Required.");
+                return;
+            }
+
+            if (TextUtils.isEmpty(password)) {
+                mPassword.setError("Password is Required.");
+                return;
+            }
+
+            if (password.length() < 6) {
+                mPassword.setError("Password Must be >= 6 Characters");
+                return;
+            }
+        });
+    }
+}
