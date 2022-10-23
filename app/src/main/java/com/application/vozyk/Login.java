920ef432024c36a438c24a413faca787eb61294b
Asiptsova <asipcova@gmail.com>
2022-10-23 19:36:53 +0200

    <version new> Login&Registration

--- app/src/main/java/com/application/vozyk/Login.java
+++ app/src/main/java/com/application/vozyk/Login.java
@@ -1,26 +1,85 @@
 package com.application.vozyk;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
 public class Login extends AppCompatActivity {
+    FirebaseAuth firebaseAuth;
+    public static final int SIGN_IN_CODE = 777;
+    FirebaseAuth.AuthStateListener firebaseAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        for (String provider : AuthUI.SUPPORTED_PROVIDERS) {
+            Log.v(this.getClass().getName(), provider);
+        }
         setContentView(R.layout.login);
+
         final EditText mEmail = findViewById(R.id.Email);
         final EditText mPassword = findViewById(R.id.password);
         final Button mLoginBtn = findViewById(R.id.loginBtn);
         final TextView  mCreateBtn = findViewById(R.id.createText);
-        final TextView forgotTextLink = findViewById(R.id.forgotPassword);
+        final TextView  forgotTextLink = findViewById(R.id.forgotPassword);
+        //   signInButton = findViewById(R.id.btn_googleLogin);
+      //  final ImageView ShowHidePWD=findViewById(R.id.show_hide_pwd);
+       /* ShowHidePWD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mPassword.getTransformationMethod().equals(HideReturnsTransformationMethod.getInstance()))
+                {
+                    mPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+                else
+                {
+                    mPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+            }
+        });*/
+
+//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestIdToken("1008459252405-t0jbvgp83tsf7qjig4k4gef16u9spm0o.apps.googleusercontent.com")
+//                .requestEmail()
+//                .build();
+//
+//        googleApiClient = new GoogleApiClient.Builder(this)
+//                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+//                .build();
+//        signInButton.setOnClickListener(v -> {
+//            Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+//            startActivityForResult(intent, SIGN_IN_CODE);
+//        });
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuthListener = firebaseAuth -> {
+            if (firebaseAuth.getCurrentUser() != null) {
+                goMainScreen();
+            }
+        };
+
         mLoginBtn.setOnClickListener(v -> {
+
             String email = mEmail.getText().toString().trim();
             String password = mPassword.getText().toString().trim();
 
@@ -38,6 +97,93 @@ public class Login extends AppCompatActivity {
                 mPassword.setError("Password Must be >= 6 Characters");
                 return;
             }
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Toast.makeText(Login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                } else {
+                    Toast.makeText(Login.this, "Error ! " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            });
+
         });
+
+        mCreateBtn.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), Registration.class)));
+
+        forgotTextLink.setOnClickListener(v -> {
+
+            final EditText resetMail = new EditText(v.getContext());
+            final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+            passwordResetDialog.setTitle("Reset Password ?");
+            passwordResetDialog.setMessage("Enter Your Email To Received Reset Link.");
+            passwordResetDialog.setView(resetMail);
+
+            passwordResetDialog.setPositiveButton("Yes", (dialog, which) -> {
+                String mail = resetMail.getText().toString();
+                FirebaseAuth.getInstance().sendPasswordResetEmail(mail).addOnSuccessListener(aVoid -> Toast.makeText(this, "Reset Link Sent To Your Email.", Toast.LENGTH_SHORT).show()).addOnFailureListener(e -> Toast.makeText(this, "Error ! Reset Link is Not Sent" + e.getMessage(), Toast.LENGTH_SHORT).show());
+
+            });
+
+            passwordResetDialog.setNegativeButton("No", (dialog, which) -> {
+            });
+            passwordResetDialog.create().show();
+
+        });
+
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseAuth.getInstance().addAuthStateListener(firebaseAuthListener);
+    }
+
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == SIGN_IN_CODE) {
+//            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+//
+//        }
+//    }
+
+//    private void firebaseAuthWithGoogle(GoogleSignInAccount signInAccount) {
+//
+//        signInButton.setVisibility(View.GONE);
+//
+//        AuthCredential credential = GoogleAuthProvider.getCredential(signInAccount.getIdToken(), null);
+//        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(this, task -> {
+//
+//            signInButton.setVisibility(View.VISIBLE);
+//
+//            if (!task.isSuccessful()) {
+//                Toast.makeText(getApplicationContext(), R.string.login_failed, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//    }
+
+    private void goMainScreen() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (firebaseAuthListener != null) {
+            FirebaseAuth.getInstance().removeAuthStateListener(firebaseAuthListener);
+        }
+
+    }
+
+
 }
---
+++ app/src/main/java/com/application/vozyk/Registration.java
@@ -0,0 +1,169 @@
+package com.application.vozyk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.regex.Pattern;
+
+public class Registration extends AppCompatActivity {
+
+    private DatabaseReference getUsersRef(String ref) {
+        return FirebaseDatabase.getInstance().getReference("Users")
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(ref);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+        final EditText mFullName = findViewById(R.id.fullName);
+        final EditText mEmail = findViewById(R.id.Email);
+        final EditText mPassword = findViewById(R.id.password);
+        final EditText etConfirm = findViewById(R.id.etConfirmPassword);
+        final Button mRegisterBtn = findViewById(R.id.register);
+        final TextView mLoginBtn = findViewById(R.id.createText);
+        final CircularProgressIndicator indicator =findViewById(R.id.progress_barCircle);
+      //  final ImageView ShowHidePWD = findViewById(R.id.show_hide_pwd);
+       /* ShowHidePWD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mPassword.getTransformationMethod().equals(HideReturnsTransformationMethod.getInstance()))
+                    mPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                else
+                    mPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            }
+        });*/
+
+        final ImageView ShowHidePWDConfirm = findViewById(R.id.show_hide_pwd_confirm);
+        ShowHidePWDConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (etConfirm.getTransformationMethod().equals(HideReturnsTransformationMethod.getInstance()))
+                    etConfirm.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                else
+                    etConfirm.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            }
+        });
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+        mRegisterBtn.setOnClickListener(v -> {
+            final String email = mEmail.getText().toString().trim();
+            String password = mPassword.getText().toString();
+            final String fullName = mFullName.getText().toString();
+            String Confirm = etConfirm.getText().toString();
+            if (TextUtils.isEmpty(email)) {
+                mEmail.setError("Email is Required.");
+                return;
+            }
+            if (!isValid(email)) {
+                mEmail.setError("Email isn't real.");
+                return;
+            }
+            if (TextUtils.isEmpty(password)) {
+                mPassword.setError("Password is Required.");
+                return;
+            }
+            if (password.length() >= 8 && upperCase(password) && lowerCase(password) && numberCase(password) && specialCase(password)) {
+                System.out.println("Password accepted");
+            } else {
+                mPassword.setError("Password contains 8 characters,upper & lower letters,number and special character");
+                return;
+            }
+            if (TextUtils.isEmpty(Confirm)) {
+                etConfirm.setError("Confirm Password is Required.");
+                return;
+            }
+            if (!mPassword.getText().toString().equals(etConfirm.getText().toString())) {
+                etConfirm.setError("Password & Confirm Password must be same");
+                return;
+            }
+            indicator.show();
+            indicator.setVisibility(View.VISIBLE);
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    FirebaseAuth.getInstance().getCurrentUser().sendEmailVerification()
+                            .addOnSuccessListener(aVoid ->
+                                    Toast.makeText(this, "Verification Email Has been Sent.", Toast.LENGTH_SHORT).show()).addOnFailureListener(e -> Log.d("TAG", "onFailure: Email not sent " + e.getMessage()));
+                    getUsersRef("name").setValue(fullName);
+                    getUsersRef("email").setValue(email);
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
+            });
+        });
+        mLoginBtn.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), Login.class)));
+    }
+    private boolean upperCase(String str) {
+        char ch;
+        for (int i = 0; i < str.length(); i++) {
+            ch = str.charAt(i);
+            if (Character.isUpperCase(ch)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean lowerCase(String str) {
+        char ch;
+        for (int i = 0; i < str.length(); i++) {
+            ch = str.charAt(i);
+            if (Character.isLowerCase(ch)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean numberCase(String str) {
+        char ch;
+        for (int i = 0; i < str.length(); i++) {
+            ch = str.charAt(i);
+            if (Character.isDigit(ch))
+                return true;
+        }
+        return false;
+    }
+
+    private boolean specialCase(String str) {
+        char ch;
+        char ph;
+        String specialChars = "/0*!@$^&*()\"{}_[]|\\?<>,.";
+        for (int i = 0; i < str.length(); i++) {
+            ch = str.charAt(i);
+            for (int j = 0; j < specialChars.length(); j++) {
+                ph = specialChars.charAt(j);
+                if (ch == ph) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isValid(String email) {
+        String emailRegex =  "^([_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*(\\.[a-zA-Z]{1,6}))?$";
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
+}
