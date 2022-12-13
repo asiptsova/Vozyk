f23f4e616bf7cb29367d763c10a52c6a5751944d
Asiptsova <asipcova@gmail.com>
2022-12-13 21:42:23 +0100

    <fixed>

--- app/src/main/java/com/application/vozyk/ui/other/Other.java
+++ app/src/main/java/com/application/vozyk/ui/other/Other.java
@@ -11,7 +11,6 @@ import androidx.fragment.app.Fragment;
 import com.application.vozyk.ui.about.About_us;
 import com.application.vozyk.ui.account.Account;
 import com.application.vozyk.R;
-import com.application.vozyk.ui.habits.Habits;
 import com.application.vozyk.ui.notes.NotesListActivity;
 import com.application.vozyk.ui.quiz.QuizActivity;
 import com.application.vozyk.ui.ToDoList.ToDoListActivity;
@@ -20,14 +19,12 @@ public class Other extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_other, container, false);
-        final Button habit_link=root.findViewById(R.id.habit_link);
         final Button account=root.findViewById(R.id.account);
         final Button about=root.findViewById(R.id.about);
         final Button quiz=root.findViewById(R.id.test);
         final Button toDo=root.findViewById(R.id.toDo);
         final Button notes=root.findViewById(R.id.notes);
 
-        habit_link.setOnClickListener(v -> startActivity(new Intent(getContext(), Habits.class)));
 
         account.setOnClickListener(v -> {
             Intent intent = new Intent(getContext(), Account.class);
